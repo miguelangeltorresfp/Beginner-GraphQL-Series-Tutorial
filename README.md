edce751da56144b216050a5d80bcab6e61c20174
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2019-07-08 18:01:00 +0200

    Basic types

--- README.md
+++ README.md
@@ -9,3 +9,21 @@
 * Install Apollo Server & GraphQL - `yarn add apollo-server graphql`
 
 * Create scr/index.js file
+
+## PART 1 - BASIC TYPES
+
+* `yarn add -D node-dev`
+
+* Link to the GraphQL Cheat Sheet - [LINK](https://github.com/sogko/graphql-schema-language-cheat-sheet)
+
+* To pass parameters or arguments to queries you have do it with input
+
+```bash
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+```
+
+* Mutations run in order whereas queries run in parallel
--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     hello: String!
+    user: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
+  type RegisterResponse {
+    errors: [Error]
+    user: User
+  }
+
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hello world"
+    hello: () => "hello world",
+    user: () => ({ id: 1, username: "bob" })
+  },
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [{ field: "username", message: "bad" }],
+      user: { id: 1, username: "bob" }
+    })
   }
 };
 
