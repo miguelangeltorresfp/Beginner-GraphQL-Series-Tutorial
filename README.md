47753af9a32900fd255131e472d34e1ca1b05c31
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2019-07-09 13:41:10 +0200

    3_graphql_subscriptions

--- README.md
+++ README.md
@@ -113,3 +113,72 @@ const resolvers = {
   }
 }
 ```
+
+## PART 3 - HOW GRAPHQL SUBSCRIPTIONS WORK
+
+* First we create de type definition:
+
+```bash
+const typeDefs = gql`
+  type Subscription {
+    newUser: User!
+  }
+`;
+```
+
+* And we can trigger an event using PubSub from Apollo Server.
+We have to pass the pubsub instance to the context:
+
+```bash
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res, pubsub })
+});
+```
+
+* Then we have to create a subscription in the resolver using the subscribe function which receive 4 parameters:
+
+```bash
+const resolvers = {
+  Subscription: {
+    newUser: {
+      subscribe: (parent, args, context) => {}
+    }
+  }
+}
+```
+
+```bash
+const NEW_USER = "NEW_USER";
+
+const resolvers = {
+  Subscription: {
+    newUser: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator(NEW_USER)
+    }
+  }
+}
+```
+
+* Then we trigger the event using another resolver:
+
+```bash
+const resolvers = {
+  Mutation: {
+    register: (_, { userInfo: { username } }, { pubsub }) => {
+      const user = {
+        id: 1,
+        username
+      };
+      pubsub.publish(NEW_USER, { newUser: user });
+      return {
+        errors: [{ field: "username", message: "bad" }],
+        user
+      };
+    }
+  }
+};
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, PubSub } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
@@ -18,7 +18,7 @@ const typeDefs = gql`
   }
 
   type RegisterResponse {
-    errors: [Error]
+    errors: [Error!]!
     user: User
   }
 
@@ -32,9 +32,20 @@ const typeDefs = gql`
     login(userInfo: UserInfo!): String!
     register(userInfo: UserInfo!): RegisterResponse!
   }
+
+  type Subscription {
+    newUser: User!
+  }
 `;
 
+const NEW_USER = "NEW_USER";
+
 const resolvers = {
+  Subscription: {
+    newUser: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator(NEW_USER)
+    }
+  },
   User: {
     // username: () => "I am username",
     firstLetterOfUserName: parent => {
@@ -56,17 +67,26 @@ const resolvers = {
       console.log(context);
       return username;
     },
-    register: () => ({
-      errors: [{ field: "username", message: "bad" }],
-      user: { id: 1 }
-    })
+    register: (_, { userInfo: { username } }, { pubsub }) => {
+      const user = {
+        id: 1,
+        username
+      };
+      pubsub.publish(NEW_USER, { newUser: user });
+      return {
+        errors: [{ field: "username", message: "bad" }],
+        user
+      };
+    }
   }
 };
+
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req, res }) => ({ req, res })
+  context: ({ req, res }) => ({ req, res, pubsub })
 });
 
 server.listen().then(({ url }) => console.log(`Server started at ${url}`));
